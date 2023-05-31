5e03bdb6202f0ec6e2e499e7781237b4d3d54552
Aaron Seibert <aaron@binaryitsystems.com>
2023-05-31 13:48:57 -0400

    Add .config/nvim/init.lua Add .config/nvim/lua/user/keymaps.lua Add .config/nvim/lua/user/lsp.lua Add .config/nvim/lua/user/lualine.lua Add .config/nvim/lua/user/nvim-tree.lua Add .config/nvim/lua/user/plugins.lua Add .config/nvim/plugin/packer_compiled.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,22 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("user.plugins")
+require("user.nvim-tree")
+require("user.keymaps")
+require("user.lualine")
+vim.cmd([[
+	filetype plugin indent on
+	set ts=2 sw=4
+	set expandtab
+	set smarttab
+	set autoindent
+	set nu rnu
+	set inccommand=nosplit
+	colorscheme nord
+]])
+
---
+++ dot_config/nvim/lua/user/nvim-tree.lua
@@ -0,0 +1,76 @@
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+local function open_nvim_tree()
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
+-- Call our function to open nvim_tree on VimEnter
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- empty setup using defaults
+nvim_tree.setup {
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  renderer = {
+    root_folder_modifier = ":t",
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  view = {
+    width = 30,
+    side = "left",
+    mappings = {
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
+      },
+    },
+  },
+}
