786c5b774603fdcd1f30df12b8265e78142ffa57
Aaron Seibert <aaron@awseibert.net>
2024-06-29 22:30:03 -0400

    Update .config/nvim/lua/user/plugins.lua

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -52,7 +52,17 @@ return require('packer').startup(function(use)
 	use {'arcticicestudio/nord-vim'}
 	use {'qpkorr/vim-bufkill'}
 	use {'hashivim/vim-terraform'}
-  use {'nvim-tree/nvim-tree.lua', requires = 'nvim-tree/nvim-web-devicons' }
+  --  use {'nvim-tree/nvim-tree.lua', requires = 'nvim-tree/nvim-web-devicons' }
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
   use {'nvim-lualine/lualine.nvim'}
 
   -- LSP
